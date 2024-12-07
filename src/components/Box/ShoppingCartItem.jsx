08496be5741d8b08661b449aa56c3f5af031ebf5
mohammad <mohammadakhlaghi843@gmail.com>
2024-12-07 12:05:29 +0100

    Shopping basket developed in topbar

---
+++ src/components/Box/ShoppingCartItem.jsx
@@ -0,0 +1,40 @@
+import CloseIcon from '@mui/icons-material/Close';
+import { digitsEnToFa } from "@persian-tools/persian-tools";
+export default function ShoppingCartItem() {
+  return (
+    <div className="cart-item flex items-start py-[1.5rem] px-[1.5rem] justify-between">
+      <div className="cart-item__right-section flex">
+        <img
+          className="w-[65px] h-[75px] object-cover"
+          src="/images/games/Forza.webp"
+        />
+        <div className="cart-item__details pr-4 flex flex-col gap-3">
+          <h4 className="cart-item__title font-bold">اسپایدرمن</h4>
+          <div className="cart-counter__controller flex items-center gap-2 text-center">
+            <span className="outline w-[15px] outline-[#77777733] rounded-lg hover:bg-purple transition-all delay-100 cursor-pointer">
+              -
+            </span>
+            <span className="outline w-[15px] outline-[#77777733] rounded-lg hover:bg-purple transition-all delay-100 cursor-pointer">
+              {digitsEnToFa(1)}
+            </span>
+            <span className="outline w-[15px] outline-[#77777733] rounded-lg hover:bg-purple transition-all delay-100 cursor-pointer">
+              +
+            </span>
+          </div>
+          <div className="price-x-count flex flex-row-reverse gap-2">
+            <span className="price text-[12px] ">
+              {digitsEnToFa(110000)}تومان
+            </span>
+            <span className="text-[12px] ">x</span>
+            <span className="count text-[#777] text-[12px] ">
+              {digitsEnToFa(1)}
+            </span>
+          </div>
+        </div>
+      </div>
+      <div className="cart-item__left-section">
+        <CloseIcon className="text-[#777777] w-[20px]" />
+      </div>
+    </div>
+  );
+}
--- src/components/Header/Topbar.jsx
+++ src/components/Header/Topbar.jsx
@@ -3,16 +3,23 @@ import WorkOutlineIcon from "@mui/icons-material/WorkOutline";
 import FavoriteBorderIcon from "@mui/icons-material/FavoriteBorder";
 import ShoppingCartIcon from "@mui/icons-material/ShoppingCart";
 import PersonIcon from "@mui/icons-material/Person";
-import MenuIcon from '@mui/icons-material/Menu';
+import MenuIcon from "@mui/icons-material/Menu";
 import Menu from "../Menu/Menu";
+import WorkOffIcon from "@mui/icons-material/WorkOff";
+import { digitsEnToFa } from "@persian-tools/persian-tools";
+import CloseIcon from "@mui/icons-material/Close";
 import { Link } from "react-router-dom";
+import { useState } from "react";
+import ShoppingCartItem from "../Box/ShoppingCartItem";
 
 export default function Topbar() {
+  const [shoppingBasket, setShoppingBasket] = useState([]);
+
   return (
     <div className="Topbar bg-primary lg:p-5">
       <div className="Topbar-wrapper container flex items-center justify-between">
         <div className="block lg:hidden">
-         <MenuIcon className="cursor-pointer"/>
+          <MenuIcon className="cursor-pointer" />
         </div>
         <div className="topbar-right__section flex items-center gap-5 lg:w-[50%]">
           <Link to={"/"} className="inline-block	max-w-[150px] lg:min-w-[160px]">
@@ -36,24 +43,66 @@ export default function Topbar() {
         </div>
         <div className="topbar-left__section flex lg:w-[50%] justify-end">
           <div className="menus hidden lg:flex items-center justify-center gap-4 ml-10">
-            <Menu title={"محصولات"} href={'/products'}>
-              <WorkOutlineIcon  className="text-purple cursor-pointer"/>
+            <Menu title={"محصولات"} href={"/products"}>
+              <WorkOutlineIcon className="text-purple cursor-pointer" />
             </Menu>
-            <Menu title={"مورد علاقه ها"} href={'/favourites'}>
+            <Menu title={"مورد علاقه ها"} href={"/favourites"}>
               <FavoriteBorderIcon className="text-pink cursor-pointer" />
             </Menu>
           </div>
 
           <div className="access-menus flex items-center justify-center gap-4">
-            <div className="shopping-icon relative">
-              <ShoppingCartIcon className='text-2xl cursor-pointer'/>
-              <div className='w-4 h-4 bg-purple rounded-lg absolute top-[-5px] right-[-10px] text-white text-center flex items-center justify-center'>
-                <span className='inline-block w-full text-[7px]'>0</span>
+            <div className="shopping-icon relative flex items-center gap-4 group">
+              <Link to="/products">
+                <ShoppingCartIcon className="text-2xl cursor-pointer" />
+              </Link>
+              <div className="w-4 h-4 bg-purple rounded-lg absolute top-[-5px] right-[-10px] text-white text-center flex items-center justify-center">
+                <span className="inline-block w-full text-[7px]">0</span>
               </div>
-            </div>
 
-            <div className="person-icon hidden lg:block">
-              <PersonIcon className='text-2xl cursor-pointer'/>
+              {shoppingBasket.length ? (
+                <>
+                  <div
+                    className="shop-basket group-hover:opacity-100 group-hover:z-50 group-hover:visible opacity-0 -z-50 invisible 
+                  group-hover:pt-0 transition-all delay-100 duration-300  
+                  overflow-y-auto  flex flex-col  absolute w-[300px] h-[225px] bg-secondary 
+                  top-[3rem] rounded-md -right-[18rem] xs:-right-[17rem] left-0
+                  [&::-webkit-scrollbar]:w-2
+                [&::-webkit-scrollbar-track]:bg-secondary
+                [&::-webkit-scrollbar-thumb]:bg-gray-300
+                  "
+                  >
+                    <ShoppingCartItem />
+                    <ShoppingCartItem />
+                    <ShoppingCartItem />
+                    <div className="plusAllPrices-wrapper flex items-center justify-between mx-6 py-3 border-y border-[#77777733]">
+                      <span className="allprices-title font-bold">جمع کل:</span>
+                      <span className="allprices-count">
+                        {digitsEnToFa(110000)}تومان
+                      </span>
+                    </div>
+                  </div>
+                </>
+              ) : (
+                <>
+                  <div className="shop-basket group-hover:opacity-100 group-hover:z-50 group-hover:visible opacity-0 -z-50 invisible group-hover:pt-0 transition-all delay-100 duration-300   flex flex-col items-center justify-center absolute w-[300px] overflow-y-auto min-h-[225px] bg-secondary top-[3rem] rounded-md -right-[18rem] xs:-right-[17rem] left-0">
+                    <WorkOffIcon className="w-[75px] h-[75px] mb-4 text-[#77777733]" />
+                    <span className="font-bold text-[18px] mb-6">
+                      هیچ محصولی در سبد خرید نیست
+                    </span>
+                    <Link
+                      to={"/"}
+                      className="bg-purple px-4 py-2 shadow-box hover:shadow-boxHover font-bold text-[12px] rounded-3xl"
+                    >
+                      بازگشت به فروشگاه{" "}
+                    </Link>
+                  </div>
+                </>
+              )}
+
+              <div className="person-icon hidden lg:block">
+                <PersonIcon className="text-2xl cursor-pointer" />
+              </div>
             </div>
           </div>
         </div>
